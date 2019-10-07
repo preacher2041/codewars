f4f68d7fc0b03e147106c32b3fc5977481abde64
leehitchcock <lee.hitchcock2041@gmail.com>
2019-10-07 18:43:06 +0100

    [LH] Added new tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
-import positiveSum from './8kyu/2-positiveSum/positiveSum';
-
-const array = [1,-2,3,4,5];
-
 function init() {
-    console.log('Positive Sum: ', positiveSum(array));
+
 }
 
 (function () {
---
+++ src/tests/8kyuTests/1-multiply.test.js
@@ -0,0 +1,5 @@
+import multiply from '../../js/8kyu/1-multiply/multiply';
+
+test('multiplies two numbers together', () => {
+    expect(multiply(4, 5)).toBe(20);
+});
